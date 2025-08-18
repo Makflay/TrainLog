188010c917561a0f9fd472559c39adaf2d9ff117
Viktor (Makflay) <chief.makflay1@gmail.com>
2025-08-18 14:53:08 +0200

    Server first create&test start

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI;
+
+app.use(express.json());
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.send('TrainLog server is running!');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
