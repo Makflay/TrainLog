270640b21136ae20a5ac32cc97f5bc1c7dbd1136
Viktor (Makflay) <chief.makflay1@gmail.com>
2025-08-18 15:20:05 +0200

    Created userSchema&auth&jwt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoUri = process.env.MONGO_URI;
@@ -15,9 +17,11 @@ mongoose.connect(mongoUri, {
 .then(() => console.log('MongoDB connected!'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+//routes
 app.get('/', (req, res) => {
   res.send('TrainLog server is running!');
 });
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
